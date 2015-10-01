764a875d772b9ce97ea8e3ad28475a6d76a70348
unknown <lili10@697-334-Edoras.sf.k12.internal>
2015-10-01 08:12:15 -0700

    Added first name to index.html

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
  Bacteria[] cars = new Bacteria [10];
  int bacteriaX = ((int)(Math.random()*401));
  int bacteriaY = ((int)(Math.random()*401)); 
- void setup()   
+ public void setup()   
  {     
  	//initialize bacteria variables here
  	for(int i=0; i<10; i++)
@@ -10,7 +26,7 @@
  	}
  	size(500,500);   
  }   
- void draw()   
+ public void draw()   
  {
  	background(0);
  	//move and show the bacteria   
@@ -29,7 +45,7 @@
  		bacteriaX = 200*(int)(Math.random()*10)-5;
  		bacteriaY = 200*(int)(Math.random()*10)-5;
  	}
- 	void walk()
+ 	public void walk()
  	{
  		bacteriaX += (int)(Math.random()*5)-2;
  		bacteriaY += (int)(Math.random()*5)-2;
@@ -43,9 +59,18 @@
  		}
  			
  	}
- 	void show()
+ 	public void show()
  	{
  		fill(255);
  		ellipse(bacteriaX, bacteriaY,15,15);
  	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
